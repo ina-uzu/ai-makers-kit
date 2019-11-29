f5eeb3f89f5f3e73c09def27fd02d26241da3616
ina-uzu <ina.uzu.song@gmail.com>
2019-11-29 15:11:10 +0900

    test code added

--- python3/example_apps/proj1_stc/eva_api.py
+++ python3/example_apps/proj1_stc/eva_api.py
@@ -1,16 +1,20 @@
 import requests
 
-url = "http://15.164.50.174:5000/"
+#url = "http://15.164.50.174:5000/"
+url = "http://127.0.0.1:5000/"
 
 
 def get_final_cmd(stt, voice):
+
     params = {"stt": stt, "voice": voice}
+
     try:
         response = requests.post(url + "cmd", params=params)
 
         print("url : ", url + "cmd")
         print("params : ", params)
         print("status code :", response.status_code)
+        return response.text
 
     except Exception as e:
         print("ERROR! ", str(e))
@@ -29,24 +33,11 @@ def send_stt(stt):
         print("ERROR! ", str(e))
 
 
-def get_final_cmd(stt, voice):
-    params = {"stt": stt, "voice": voice}
-
-    try:
-        response = requests.post(url + "eva", params=params)
-
-        print("url : ", url + "eva")
-        print("params : ", params)
-        print("status code :", response.status_code)
-
-    except Exception as e:
-        print("ERROR! ", str(e))
-
-
 def main():
     stt = input("stt: ")
 
-    get_final_cmd(stt, None)
+    cmd = get_final_cmd(stt, "TEST")
+    print(cmd)
 
 
 if __name__ == "__main__":
